d0571fa2ca3c0fc35bcaa39fdf5f71cf143f31b9
InfiniteCoder <info@infinitecoder.org>
2024-12-30 14:18:53 +0300

    Delete src/arch/x86/x32/mod.rs

--- src/arch/x86/x32/mod.rs
+++
@@ -1,7 +0,0 @@
-core::arch::global_asm!(include_str!("bootstrap.S"), options(att_syntax));
-
-pub mod early_logger;
-
-// mod paging;
-// pub(super) use paging::ks_phys_addr;
-// pub use paging::mmap;
